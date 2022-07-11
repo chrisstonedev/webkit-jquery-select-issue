080d2dbdb16d4cd5878e7c74d219c1ae04dcb545
Chris Stone <contact@chrisstone.dev>
2022-07-11 00:50:53 -0400

    Added tests for attr() and prop() methods

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,8 +1,17 @@
 import {test, expect} from '@playwright/test';
 
-test('should allow me to add todo items', async ({page}) => {
-    await page.goto('http://localhost:8080');
-    await page.click('#changeOption');
-    await page.click('#submitForm');
-    await expect(page.locator('#parameters')).toHaveText('selectDropDown=A');
+test.describe('Select element should always only return one value', () => {
+    test('when using the .attr() method', async ({page}) => {
+        await page.goto('http://localhost:8080');
+        await page.click('#changeOptionAttr');
+        await page.click('#submitForm');
+        await expect(page.locator('#parameters')).toHaveText('selectDropDown=A');
+    });
+
+    test('when using the .prop() method', async ({page}) => {
+        await page.goto('http://localhost:8080');
+        await page.click('#changeOptionProp');
+        await page.click('#submitForm');
+        await expect(page.locator('#parameters')).toHaveText('selectDropDown=A');
+    });
 });
